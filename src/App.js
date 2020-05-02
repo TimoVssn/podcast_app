9ed7bd3a6ede1e9c189aaca8892bd53bc694e79e
TimoVssn <timovossen@gmail.com>
2020-05-02 13:45:15 +0200

    First commit
    Uploaded all the files.

---
+++ src/App.js
@@ -0,0 +1,68 @@
+import React, {Component} from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+let defaultTextColor = '#ffffff';
+let defaultStyle = {color: defaultTextColor, 'font-size': '8px'};
+
+class Categorie extends Component {
+  render(){
+    return(
+      <div style={{width:'25%', display: 'inline-block'}}>
+        <img/>
+        <h2 style={{...defaultStyle, 'font-size': '15px'}}>Text</h2>
+      </div>
+    );
+  }
+}
+
+class Playlist extends Component {
+  render(){
+    return(
+      <div style={{width:'33%', display: 'inline-block'}}>
+        <img/>
+        <h2 style={{...defaultStyle, 'font-size': '15px'}}>Titel</h2>
+        <p style={{...defaultStyle}}>Beschrijving</p>
+      </div>
+    );
+  }
+}
+
+class NewPodcast extends Component {
+  render(){
+    return(
+      <div style={{'background-color': 'green', width: '100%', height: '50px'}}>
+          <img style={{display: 'inline-block'}}/>
+          <h2 style={{...defaultStyle, 'font-size': '15px', display: 'inline-block'}}>Podcast titel</h2>
+          <div style={{display: 'inline-block'}}>
+            <p style={{...defaultStyle}}>Beschrijving van de podcast.</p>
+            <p style={{...defaultStyle}}>Datum van release</p>
+          </div>
+          <img style={{display: 'inline-block'}}/>
+      </div>
+    );
+  }
+}
+
+function App() {
+  return (
+    <div className="App">
+        <h1>Podcast</h1>
+        <h3 style={{color: defaultTextColor}}>Categories</h3>
+        <Categorie/>
+        <Categorie/>
+        <Categorie/>
+        <Categorie/>
+        <h3 style={{color: defaultTextColor}}>You're playlists</h3>
+        <Playlist/>
+        <Playlist/>
+        <Playlist/>
+        <h3 style={{color: defaultTextColor}}>New podcasts</h3>
+        <NewPodcast/>
+        <NewPodcast/>
+        <NewPodcast/>      
+    </div>
+  );
+}
+
+export default App;
